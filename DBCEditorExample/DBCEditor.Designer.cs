jacobtonder/DBCEditorExample

commit cadf3dd38ce50d977a3861e52760c15e3e3de8f7

DBCEditor: Catch exception FileNotFoundException when loading dbc

DBCEditorExample/DBCEditor.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -30,6 +31,11 @@ namespace DBCEditorExample
             catch (InvalidSignatureException)
             {
                 MessageBox.Show("Invalid DBC Signature: " + DBCStores.Signature);
+                Close();
+            }
+            catch (FileNotFoundException ex)
+            {
+                MessageBox.Show("DBC file not found: " + ex.Message);
                 Close();
             }
 

DBCEditorExample/DataStores/DBCStores.cs
@@ -15,7 +15,7 @@ namespace DBCEditorExample.DataStores
         {
             string dbcFolder = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location) + "\\DBCFiles\\";
 
-            Users = new DBCFile<Users>(dbcFolder + "Users.dbc", Signature);
+            Users = new DBCFile<Users>(dbcFolder + "Userss.dbc", Signature);
         }
     }
 }

commit 285412048ac777521f0d9364e23f67da5e400f9c

DBCEditor: Added buttons for adding and deleting entries + fixed error caused in https://github.com/jacobtonder/DBCEditorExample/commit/cadf3dd38ce50d977a3861e52760c15e3e3de8f7

DBCEditorExample/DBCEditor.Designer.cs
@@ -29,6 +29,8 @@
         private void InitializeComponent()
         {
             this.ListBox_Users = new System.Windows.Forms.ListBox();
+            this.Button_Add = new System.Windows.Forms.Button();
+            this.Button_Delete = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // ListBox_Users
@@ -36,14 +38,36 @@
             this.ListBox_Users.FormattingEnabled = true;
             this.ListBox_Users.Location = new System.Drawing.Point(13, 13);
             this.ListBox_Users.Name = "ListBox_Users";
-            this.ListBox_Users.Size = new System.Drawing.Size(226, 355);
+            this.ListBox_Users.Size = new System.Drawing.Size(156, 329);
             this.ListBox_Users.TabIndex = 0;
+            // 
+            // Button_Add
+            // 
+            this.Button_Add.Location = new System.Drawing.Point(13, 349);
+            this.Button_Add.Name = "Button_Add";
+            this.Button_Add.Size = new System.Drawing.Size(75, 23);
+            this.Button_Add.TabIndex = 1;
+            this.Button_Add.Text = "Add";
+            this.Button_Add.UseVisualStyleBackColor = true;
+            this.Button_Add.Click += new System.EventHandler(this.Button_Add_Click);
+            // 
+            // Button_Delete
+            // 
+            this.Button_Delete.Location = new System.Drawing.Point(94, 349);
+            this.Button_Delete.Name = "Button_Delete";
+            this.Button_Delete.Size = new System.Drawing.Size(75, 23);
+            this.Button_Delete.TabIndex = 2;
+            this.Button_Delete.Text = "Delete";
+            this.Button_Delete.UseVisualStyleBackColor = true;
+            this.Button_Delete.Click += new System.EventHandler(this.Button_Delete_Click);
             // 
             // DBCEditor
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(557, 373);
+            this.ClientSize = new System.Drawing.Size(557, 380);
+            this.Controls.Add(this.Button_Delete);
+            this.Controls.Add(this.Button_Add);
             this.Controls.Add(this.ListBox_Users);
             this.Name = "DBCEditor";
             this.Text = "DBC Editor";
@@ -55,6 +79,8 @@
         #endregion
 
         private System.Windows.Forms.ListBox ListBox_Users;
+        private System.Windows.Forms.Button Button_Add;
+        private System.Windows.Forms.Button Button_Delete;
     }
 }
 

DBCEditorExample/DBCEditor.cs
@@ -40,10 +40,58 @@ namespace DBCEditorExample
             }
 
             ListBox_Users.Items.Clear();
-            foreach (Users t in DBCStores.Users.Records)
-                ListBox_Users.Items.Add(t);
+            foreach (Users user in DBCStores.Users.Records)
+                ListBox_Users.Items.Add(user);
 
             ListBox_Users.SelectedIndex = 0;
+        }
+
+        private void Button_Add_Click(object sender, EventArgs e)
+        {
+            if (ListBox_Users.SelectedIndex == -1)
+                return;
+
+            Users user = new Users()
+            {
+                Id = DBCStores.Users.MaxKey + 1,
+                FirstName = "New",
+                LastName = "User",
+                BirthYear = 1970,
+                Email = ""
+            };
+
+            try
+            {
+                ListBox_Users.Items.Add(user);
+                DBCStores.Users.AddEntry(user.Id, user);
+
+                // Select the new entry
+                ListBox_Users.SelectedIndex = ListBox_Users.Items.Count - 1;
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+        }
+
+        private void Button_Delete_Click(object sender, EventArgs e)
+        {
+            if (ListBox_Users.SelectedIndex == -1)
+                return;
+
+            try
+            {
+                Users user = (Users)ListBox_Users.Items[ListBox_Users.SelectedIndex];
+
+                --ListBox_Users.SelectedIndex;
+
+                DBCStores.Users.RemoveEntry(user.Id);
+                ListBox_Users.Items.Remove(user);
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
         }
     }
 }

DBCEditorExample/DataStores/DBCStores.cs
@@ -15,7 +15,7 @@ namespace DBCEditorExample.DataStores
         {
             string dbcFolder = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location) + "\\DBCFiles\\";
 
-            Users = new DBCFile<Users>(dbcFolder + "Userss.dbc", Signature);
+            Users = new DBCFile<Users>(dbcFolder + "Users.dbc", Signature);
         }
     }
 }