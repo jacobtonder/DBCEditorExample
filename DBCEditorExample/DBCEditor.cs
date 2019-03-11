b379e153e8ca90ca1488c3f8dd44c1248925625a
Jacob Tønder <jacob@softwarekonsulenten.dk>
2019-03-11 08:37:40 +0100

    DBCEditor: Removed unnecessary usings

--- DBCEditorExample/DBCEditor.cs
+++ DBCEditorExample/DBCEditor.cs
@@ -1,16 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using DBCEditorExample.DataStores;
 using DBCEditorExample.Structures;
-using DBCLib;
 using DBCLib.Exceptions;
 
 namespace DBCEditorExample
